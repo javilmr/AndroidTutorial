82e55f439fa1742240fa3ffc8248ec5f0af060da
Javier Lopez-Montenegro Ramil <javier.lopez-montenegroramil@epfl.ch>
2012-09-21 19:43:33 +0200

    first commit

---
+++ src/com/example/myfirstandriodapp/ShowMessageActivity.java
@@ -0,0 +1,26 @@
+package com.example.myfirstandriodapp;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.widget.TextView;
+
+public class ShowMessageActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_message);
+        Intent startingIntent=getIntent();
+        String userText=startingIntent.getStringExtra(MainActivity.class.getName());
+        TextView textView = (TextView)findViewById(R.id.diplayed_text);
+        textView.setText(userText);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_show_message, menu);
+        return true;
+    }
+}
